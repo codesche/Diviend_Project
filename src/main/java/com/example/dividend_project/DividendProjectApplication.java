16285761dba8958ad331f064668840db06bf5074
codesche <codesche@gmail.com>
2022-12-28 23:05:23 +0900

    first commit: Project Setting

---
+++ src/main/java/com/example/dividend_project/DividendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dividend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendProjectApplication.class, args);
+    }
+
+}
