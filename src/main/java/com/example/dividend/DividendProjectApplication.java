ecd21ffa43609ab4995f8f05d4dfbc3607dcbea1
hms7709 <hms7709@naver.com>
2023-01-01 22:23:59 +0900

    feat: Scraping 로직 구현

--- src/main/java/com/example/dividend/DividendProjectApplication.java
+++ src/main/java/com/example/dividend/DividendProjectApplication.java
@@ -14,36 +14,13 @@ public class DividendProjectApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(DividendProjectApplication.class, args);
 
-        // 스크래핑 구현
-        try {
-            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?"
-                + "period1=1640649705&period2=1672185705&interval=1wk&filter="
-                + "history&frequency=1wk&includeAdjustedClose=true");
-            Document document = connection.get();
+        String s = "Hello my name is %s";
 
-            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element ele = eles.get(0);     // table 전체
+        String[] names = {"GREEN", "RED", "BLUE"};
 
-            Element tbody = ele.children().get(1);
-            for (Element e: tbody.children()) {
-                String txt = e.text();
-                if (!txt.endsWith("Dividend")) {
-                    continue;
-                }
-
-                String[] splits = txt.split(" ");
-                String month = splits[0];
-                int day = Integer.valueOf(splits[1].replace(",", ""));
-                int year = Integer.valueOf(splits[2]);
-                String dividend = splits[3];
-
-                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (String name : names) {
+            System.out.println(String.format(s, name));
         }
-
 
     }
 
