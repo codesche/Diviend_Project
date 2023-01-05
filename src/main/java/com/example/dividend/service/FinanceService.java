fdf8f99053edaa30a9740a593cbf503f1c36547f
codesche <codesche@gmail.com>
2023-01-06 00:05:07 +0900

    feat: 캐시 기능 추가
    Cacheable 어노테이션 통한 cache 구현

--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -12,8 +12,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -21,8 +24,9 @@ public class FinanceService {
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
 
+    @Cacheable(key = "#companyName", value = "finance")
     public ScrapedResult getDividendByCompanyName(String companyName) {
-
+        log.info("search company -> " + companyName);
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
                                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다"));
@@ -31,25 +35,11 @@ public class FinanceService {
         List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
 
         // 3. 결과 조합 후 반환
-//        List<Dividend> dividends = new ArrayList<>();
-//        for (var entity : dividendEntities) {
-//            dividends.add(Dividend.builder()
-//                    .date(entity.getDate())
-//                    .dividend(entity.getDividend())
-//                    .build());
-//        }
-
         List<Dividend> dividends = dividendEntities.stream()
-                                                    .map(e -> Dividend.builder()
-                                                        .date(e.getDate())
-                                                        .dividend(e.getDividend())
-                                                        .build())
+                                                    .map(e -> new Dividend(e.getDate(), e.getDividend()))
                                                     .collect(Collectors.toList());
 
-        return new ScrapedResult(Company.builder()
-                                        .ticker(company.getTicker())
-                                        .name(company.getName())
-                                        .build(),
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()),
                                 dividends);
     }
 
